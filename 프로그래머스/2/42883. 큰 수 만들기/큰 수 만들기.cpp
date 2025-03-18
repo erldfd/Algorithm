cf9cb17de782c23b87656b247746c477acb7c6ab
erldfd <94783387+erldfd@users.noreply.github.com>
2025-03-18 09:58:06 +0900

    [level 2] Title: 큰 수 만들기, Time: 2666.54 ms, Memory: 5.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,31 +1,44 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 string solution(string number, int k) 
 {
+    // num, index
+    vector<pair<char, int>> Rank;
     string answer = "";
-
-    int biggest = 0;
-    int idx = 0;
-
-    for (int i = 0; i < number.length(); ++i)
+    
+    for(int i = 0; i < number.size(); ++i)
     {
-        if (biggest < number[i])
+        Rank.push_back(make_pair(number[i], i));
+        sort(Rank.begin(), Rank.end(), [](const pair<char, int>& a, const pair<char, int>& b)
+             {
+                 return a.first < b.first;
+             });
+        
+        if (Rank.size() > k)
         {
-            biggest = number[i];
-            idx = i;
-        }
-
-        if (i == k)
-        {
-            i = idx;
-            ++k;
-            answer.push_back(biggest);
-            biggest = 0;
+            Rank.pop_back();
         }
     }
-
+    
+    
+    for(auto& R : Rank)
+    {
+        cout << R.first << ", " << R.second << " / ";
+    }
+    cout << endl;
+    cout << number << endl;
+    
+    for(int i =0; i < Rank.size(); ++i)
+    {
+        remove(number.begin(), number.end(), Rank[i].first);
+        number.pop_back();
+        cout << number << endl;
+    }
+    
     return answer;
 }
