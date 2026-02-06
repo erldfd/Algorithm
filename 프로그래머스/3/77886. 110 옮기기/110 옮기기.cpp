511d4c256046aa3c3795a7455658248b54dfab90
erldfd <94783387+erldfd@users.noreply.github.com>
2026-02-06 14:26:25 +0900

    [level 3] Title: 110 옮기기, Time: 75.76 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.cpp
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.cpp
@@ -1,52 +1,57 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-// 문자열에서 마지막 '0'의 위치를 찾는 함수
-int findLastZero(const string& str) {
-    for (int i = str.length() - 1; i >= 0; i--) {
-        if (str[i] == '0') return i;
+int FindLastZero(const string& str)
+{
+    for(int i = str.size() - 1; i >= 0; --i)
+    {
+        if(str[i] == '0')
+        {
+            return i;
+        }
     }
+    
     return -1;
 }
 
-vector<string> solution(vector<string> s) {
-    vector<string> answer;
-
-    for (const string& originalStr : s) {
-        string processedStr = ""; // 처리된 문자열을 저장할 변수
-        int count110 = 0; // "110"이 나타난 횟수
-
-        // 주어진 문자열에서 "110"을 찾아 제거하고 개수를 센다
-        for (char ch : originalStr) {
-            processedStr += ch;
-            if (processedStr.length() >= 3 && processedStr.substr(processedStr.length() - 3) == "110") {
-                processedStr.erase(processedStr.length() - 3);
-                count110++;
+vector<string> solution(vector<string> s) 
+{
+    for(string& str : s)
+    {
+        int Index = 0;
+        int Count = 0;
+        string Copy = "";
+        
+        for(const char c : str)
+        {
+            Copy += c;
+            if(Copy.size() >= 3 && Copy.substr(Copy.size() - 3) == "110")
+            {
+                Count++;
+                Copy.erase(Copy.size() - 3);
             }
         }
-
-        int lastZeroIndex = findLastZero(processedStr); // 마지막 '0'의 위치 찾기
-
-        string result; // 최종 문자열을 저장할 변수
-        if (lastZeroIndex == -1) {
-            // '0'이 없는 경우, "110"을 모두 앞에 추가
-            while (count110--) result += "110";
-            result += processedStr;
-        } else {
-            // '0'이 있는 경우, 마지막 '0' 뒤에 "110"을 모두 추가
-            for (int j = 0; j < processedStr.length(); j++) {
-                if (j == lastZeroIndex) {
-                    result += processedStr[j];
-                    while (count110--) result += "110";
-                } else {
-                    result += processedStr[j];
-                }
-            }
+        
+        Index = FindLastZero(Copy);
+        string Temp = "";
+        while(Count--)
+        {
+            Temp.append("110");
         }
-        answer.push_back(result);
+        
+        if(Index == -1)
+        {
+            str = Temp + Copy;
+        }
+        else
+        {
+            Copy.insert(Index + 1, Temp);
+            str = Copy;
+        }
     }
-
-    return answer;
+    
+    return s;
 }
